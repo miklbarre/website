1984c61ba9af2dfeefa7810b0b0458293005079b
Mickael Barré <miklbarre@gmail.com>
2017-08-31 16:31:26 +0200

    maj plugi,é

---
+++ src/Website/HomeBundle/WebsiteHomeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Website\HomeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebsiteHomeBundle extends Bundle
+{
+}
---
+++ src/Website/MoviesBundle/WebsiteMoviesBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Website\MoviesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebsiteMoviesBundle extends Bundle
+{
+}
