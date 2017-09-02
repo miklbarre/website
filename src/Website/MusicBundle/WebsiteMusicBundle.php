f61759182a28a06cf02f84fcb5095700e71d4ec7
Mickael Barré <miklbarre@gmail.com>
2017-09-02 10:22:36 +0200

    Maj music

---
+++ src/Website/MusicBundle/WebsiteMusicBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Website\MusicBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebsiteMusicBundle extends Bundle
+{
+}
---
+++ src/Website/SerieBundle/WebsiteSerieBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Website\SerieBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebsiteSerieBundle extends Bundle
+{
+}
