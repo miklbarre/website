c527de6fadce66babfd62e3373b7dc297374c63c
Mickael <miklbarre@gmail.com>
2020-03-01 11:12:28 +0100

    test gitkraken

--- src/Website/HomeBundle/Controller/HomeController.php
+++ src/Website/HomeBundle/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace Website\HomeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\VarDumper\VarDumper;
 
 class HomeController extends Controller {
@@ -13,22 +15,23 @@ class HomeController extends Controller {
     }
 
     public function speAction(Request $request) {
-        if($request->get('specialties_1')) {
-            $i = 1;
-            $tabSpecialties = array('specialties' => []);
-            while($request->get('specialties_'.$i)) {
-                $tabSelectSpecialties = array();
-                foreach ($request->get('specialties_'.$i) as $specialty) {
-                    $tabSelectSpecialties [] = $specialty;
-                }
-                $tabSpecialties['specialties'][] = $tabSelectSpecialties;
-                $i++;
-            }
-
-        }
-        varDumper::dump(json_encode($tabSpecialties));
-        die;
-        return new Response('toto');
+        $date_debut = "2020-02-24 10:00";
+        $date_fin = "2020-02-24 10:20";
+        $objet = "Apprendre le PHP avec éééé ";
+        $lieu = "Maison";
+        $details = "Chapitre 2: connexion à une base de donnééééées";
+        $ics = "BEGIN:VCALENDAR\n";
+        $ics .= "VERSION:2.0\n";
+        $ics .= "PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n";
+        $ics .= "BEGIN:VEVENT\n";
+        $ics .= "DTSTART:".date('Ymd',strtotime($date_debut))."T".date('His',strtotime($date_debut))."\n";
+        $ics .= "DTEND:".date('Ymd',strtotime($date_fin))."T".date('His',strtotime($date_fin))."\n";
+        $ics .= "SUMMARY:".$objet."\n";
+        $ics .= "LOCATION:".$lieu."\n";
+        $ics .= "DESCRIPTION:".$details."\n";
+        $ics .= "END:VEVENT\n";
+        $ics .= "END:VCALENDAR\n";
+        return new Response($ics);
     }
 
 }
